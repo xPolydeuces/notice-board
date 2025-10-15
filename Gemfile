36cbff126014e3848d4f0343f9868b49e7a073f9
Patryk Rogala <patryk@astarium.pl>
2025-10-15 09:23:18 +0200

    Add strong_migrations gem for enhanced migration safety; configure initial settings for timeouts, analysis, and versioning

--- Gemfile
+++ Gemfile
@@ -54,6 +54,9 @@ gem "interactor-rails"
 # Haml for HTML templates
 gem "haml-rails"
 
+# Catch unsafe migrations in development
+gem "strong_migrations"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri windows], require: "debug/prelude"
---
+++ config/initializers/strong_migrations.rb
@@ -0,0 +1,18 @@
+# Mark existing migrations as safe
+StrongMigrations.start_after = 20_251_015_072_033
+
+# Set timeouts for migrations
+# If you use PgBouncer in transaction mode, delete these lines and set timeouts on the database user
+StrongMigrations.lock_timeout = 10.seconds
+StrongMigrations.statement_timeout = 1.hour
+
+# Analyze tables after indexes are added
+# Outdated statistics can sometimes hurt performance
+StrongMigrations.auto_analyze = true
+
+# Set the version of the production database
+# so the right checks are run in development
+StrongMigrations.target_version = 17
+
+# Remove invalid indexes when rerunning migrations
+StrongMigrations.remove_invalid_indexes = true
