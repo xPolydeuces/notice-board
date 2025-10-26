1d9c7c7f2cfa4b342d1283b50cf0c5962d3cfe29
Patryk Rogala <patryk@astarium.pl>
2025-10-26 11:00:55 +0100

    Update seeds file to prevent execution in test environment; remove unused traits from role factory; streamline user role factory to reference existing roles; add roles fixture for consistent role data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,7 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+return if Rails.env.test?
+
 Role.find_or_create_by!(name: "Admin", id: Role::ADMIN_ID)
 Role.find_or_create_by!(name: "Teacher", id: Role::TEACHER_ID)
