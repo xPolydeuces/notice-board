b98f19aaa4c0898096942c52f3be25fd07a188b0
Patryk Rogala <patryk@astarium.pl>
2025-10-26 09:36:55 +0100

    Refactor role model tests to use shared examples for validations; add shared examples for common model testing patterns

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,30 +5,19 @@ SimpleCov.start "rails"
 require "spec_helper"
 ENV["RAILS_ENV"] = "test"
 require_relative "../config/environment"
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-# Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
-# that will avoid rails generators crashing because migrations haven't been run yet
-# return unless Rails.env.test?
 require "rspec/rails"
+
 # Add additional requires below this line. Rails is not loaded until this point!
 require "capybara/rails"
 require "capybara/rspec"
 require "view_component/test_helpers"
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+
+# Check shared examples for easy to use common testing patterns for various Rails features
+Rails.root.glob("spec/support/**/*.rb").sort_by(&:to_s).each { |f| require f }
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -50,6 +39,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  # Include helpers for extra testing functionality
   config.include ViewComponent::TestHelpers, type: :component
   config.include ViewComponent::SystemSpecHelpers, type: :feature
   config.include ViewComponent::SystemSpecHelpers, type: :system
@@ -62,6 +52,7 @@ RSpec::Sidekiq.configure do |config|
   config.warn_when_jobs_not_processed_by_sidekiq = false
 end
 
+# Configure shoulda-matchers for one-liner testing
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
