797fb1437207ce4d40143fa05f9dcd7740d8f485
Bartosz Kot <bartosz.kot.00@gmail.com>
2025-12-08 09:42:39 +0100

    Restore environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,5 @@
 # Load the Rails application.
 require_relative "application"
 
-# Add this right above Rails.application.initialize! in config/environment.rb
-module Rails
-  class Application
-    def initialize_with_optional_arg(*args)
-      puts "WARNING: initialize! called with args: #{args.inspect}"
-      initialize_without_optional_arg # Call the original method
-    end
-    alias_method :initialize_without_optional_arg, :initialize!
-    alias_method :initialize!, :initialize_with_optional_arg
-  end
-end
-
 # Initialize the Rails application.
 Rails.application.initialize!
